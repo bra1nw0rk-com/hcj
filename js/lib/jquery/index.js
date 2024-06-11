a39fa5ff37d26e7d2cde91ee417950ce3a80ce96
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-06-11 20:35:58 +0200

    upd

--- js/lib/jquery/index.js
+++ js/lib/jquery/index.js
@@ -1,4 +1,4 @@
-import { jQuery } from "./core/jquery.js";
+//import { jQuery } from "./core/jquery.js";
 
 //await import("/js/lib/jquery/jquery.js?v=" + Math.random() * 1000000000000000000);
 await import("/js/lib/jquery/events.js?v=" + Math.random() * 1000000000000000000);
@@ -6,8 +6,8 @@ await import("/js/lib/jquery/module.js?v=" + Math.random() * 1000000000000000000
 await import("/js/lib/jquery/sheets.js?v=" + Math.random() * 1000000000000000000);
 await import("/js/lib/jquery/table.js?v=" + Math.random() * 1000000000000000000);
 
-export { jQuery, jQuery as $ };
-export default jQuery;
+//export { jQuery, jQuery as $ };
+//export default jQuery;
 
-globalThis.jQuery = jQuery;
-globalThis.$ = jQuery;
+//globalThis.jQuery = jQuery;
+//globalThis.$ = jQuery;
