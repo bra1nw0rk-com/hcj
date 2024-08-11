2e39d587a576e929747eae48dd23c311dc02b1f1
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-11 21:15:10 +0200

    upd

--- modules/forms/login-form/index.js
+++ modules/forms/login-form/index.js
@@ -3,10 +3,10 @@ import Form from "../../../js/lib/html/Form.js";
 export default class LoginForm extends Form {
 	constructor() {
 		super();
-
 		this.css = `/modules/forms/login-form/index.css`;
 		this.title = "Login";
 		this.classes = "modal fadeIn";
+		this.id ="userLoginForm"
 		this.template = $(html`
 			<input type="text" id="username" name="username" placeholder="User" autocomplete="username" required />
 			<input type="password" id="password" name="password" placeholder="Password" autocomplete="current-password" required />
@@ -14,8 +14,6 @@ export default class LoginForm extends Form {
 		`);
 
 		$("body").html("").append(this.get());
-
-
 	}
 	init() {
 		let _this = this;
