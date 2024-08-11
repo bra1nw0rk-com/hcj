4e7099c20303acbd4eb04d520da3b5697a3a53d9
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-11 21:23:31 +0200

    upd

--- js/lib/core/USER.js
+++ js/lib/core/USER.js
@@ -1,8 +1,9 @@
 import CustomEvents from "./CustomEvents.js";
+import Module from "./Module";
 
 export default class USER {
-    events= new CustomEvents()
-    loginTimer= null
+    loginTimer= 0
+    constructor() {}
     get loginTimeout() {
         return (1000 * 60 * 20);
     }
@@ -13,29 +14,30 @@ export default class USER {
         localStorage.setItem('user.sessionId', sid);
     }
     get lastUpdate() {
-        return localStorage.getItem('user.lastUpdate') || null;
+        let result = localStorage.getItem("user.lastUpdate");
+        if (result !== null) {
+            return parseInt(result);
+        }
+        return 0;
     }
     set lastUpdate(time) {
-        localStorage.setItem('user.lastUpdate', time);
+        if (time !== null) {
+            localStorage.setItem("user.lastUpdate", time.toString());
+        }
     }
-    addEvent(eventName, callback) {
-        this.events.on(eventName, callback);
-    }
-    setLoginTimer() {
+    init() {
         let _this = this
-        clearInterval(this.loginTimer);
-        this.loginTimer = setInterval(function () {
-            if (_this.needLogin()) {
-                clearInterval(_this.loginTimer)
-                _this.events.call("needLogin");
-            }
-        }, 1000);
+        $("html").on("click mousemove mousedown keypress", "*", function (e) {
+            _this.lastUpdate = new Date().getTime();
+        });
+        this.testLogin();
     }
 
+
     needLogin() {
-        if (this.sessionId !== 'null' && this.sessionId !== null) {
-            if ((parseInt(this.lastUpdate) + this.loginTimeout) < new Date().getTime()) {
-                this.sessionId = null;
+        if (this.sessionId !== "null" && this.sessionId !== null) {
+            if (this.lastUpdate + this.loginTimeout < new Date().getTime()) {
+                //here test session in server
                 return true;
             } else {
                 return false;
@@ -46,64 +48,37 @@ export default class USER {
     }
     testLogin() {
         if (this.needLogin()) {
-            this.events.call("needLogin");
+            this.logout();
         } else {
-            this.events.call("loggedIn");
+            WS.containers.call("main");
+            this.setLoginTimer();
         }
     }
+
     update(sid) {
         this.sessionId = sid;
         this.lastUpdate = new Date().getTime();
-        this.events.call("loggedIn");
+        //this.events.call("loggedIn");
     }
     login(login, password) {
-        let _this = this
         if (login === "admin" && password === "admin") {
-            WS.ui.closeModal('#userLoginForm', function () {
-                _this.update("dslfafhkldhjakelrhalweh54sdaf54d");
+            WS.ui.closeModal("#userLoginForm", function () {
+                WS.user.update("dslfafhkldhjakelrhalweh54sdaf54d");
+                WS.user.testLogin();
             });
         } else {
-            WS.ui.clearForm('#userLoginForm');
-            WS.ui.effects.shake('#userLoginForm')
+            WS.ui.clearForm("#userLoginForm");
+            WS.ui.effects.shake("#userLoginForm");
         }
         /* create api for login*/
         return false;
     }
     logout() {
-        this.events.call("logout");
+        clearInterval(this.loginTimer);
+        this.sessionId = null;
+        this.lastUpdate = 0;
+        WS.ui.clearPage();
+        Module.call("main/forms/LoginFrom.js");
     }
-    init() {
-        let _this = this
-        this.addEvent('loggedIn', function () {
-            Module.call(`main`);
-            /*
-            setTimeout(function () {
-                $("body").addClass(WS.ui.savedTheme + '-theme');
-                //topMenu.load(); !!!!!!!!!!!
-            }, 300);
 
-             */
-            this.setLoginTimer();
-        });
-        this.addEvent('needLogin', function () {
-            console.log("ok")
-            Module.call(`forms/login-form`);
-        });
-        this.addEvent('logout', function () {
-            clearInterval(_this.loginTimer);
-            this.sessionId = null;
-            this.lastUpdate = null;
-            WS.ui.clearPage();
-            this.testLogin();
-        });
-        $('body').on("click mousemove keypress", "*", function () {
-            _this.lastUpdate = new Date().getTime();
-        }).on('click', '.submit', function (e) {
-            e.preventDefault();
-            _this.login($(this).parent().find('[name="username"]').val(), $(this).parent().find('[name="password"]').val());
-            return true;
-        });
-        this.testLogin()
-
-    }
 }
