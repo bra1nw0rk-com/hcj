c6d8bdbf31674a9e06eae6977f48d2f504f4b443
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-25 22:18:09 +0200

    upd

--- js/lib/main.js
+++ js/lib/main.js
@@ -1,11 +1,5 @@
 $(function () {
-    $("body").on('keypress','input',function (e) {
-        if (e.keyCode === 13) {
-            return false;
-        } else {
-            // your code here...
-        }
-    }).on('submit','form',function(e){
+    $("body").on('submit','form',function(e){
         e.preventDefault();
         return false;
     }).on('click','input, button',function(e){
