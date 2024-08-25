51e875decf49e8bca71582662617cdbfd82a38df
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-25 16:44:56 +0200

    upd

--- modules/animations/logo/index.js
+++ modules/animations/logo/index.js
@@ -13,7 +13,7 @@ export default class AnimationLogo extends HTMLObject  {
     }
     init() {
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(105, 45);
         this.template = renderer.domElement;
