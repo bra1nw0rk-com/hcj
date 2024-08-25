b28f643f553c870470b030e1e7c5b32bcb8ed4c7
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-25 16:38:24 +0200

    upd

--- modules/animations/logo/index.js
+++ modules/animations/logo/index.js
@@ -18,8 +18,8 @@ export default class AnimationLogo extends HTMLObject  {
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(this.template.innerWidth, this.template.innerHeight);
-        this.template.append(renderer.domElement);
+        renderer.setSize(100, 100);
+        this.template = renderer.domElement;
 
 
         camera.position.z = 100;
