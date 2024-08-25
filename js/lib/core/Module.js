f0dc3bef0855aa92a4d646092f1c79ee3b343027
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-25 23:19:29 +0200

    upd

--- js/lib/core/Module.js
+++ js/lib/core/Module.js
@@ -9,7 +9,7 @@ export default class Module {
 							if (typeof this.module == "object") {
 								resolve(this.module);
 							} else if (typeof this.module == "function") {
-								resolve((new this.module()).init());
+								resolve(new this.module());
 							}
 						} else {
 							console.log(`module ${name} hasnt default export`);
