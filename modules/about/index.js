e9989add04625efd3451182282dd4b93b2149019
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-26 19:13:31 +0200

    upd

--- modules/about/index.js
+++ modules/about/index.js
@@ -12,6 +12,7 @@ export default class AboutModal extends Form {
 		this.name ="aboutModal"
 		this.content = `
 			<div>LibreIS - Libre Information System.</div>
+			<div></div>
 			<div>OS: Linux</div>
 			<div>Backend: Java</div>
 			<div>Frontend: JavaScript, JQuery, HTML, CSS</div>
