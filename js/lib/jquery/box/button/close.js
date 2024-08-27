3722b596e4509c9a545741cdadfdb86c4c54bb0d
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-27 22:28:11 +0200

    upd

--- js/lib/jquery/box/button/close.js
+++ js/lib/jquery/box/button/close.js
@@ -15,8 +15,8 @@ $(function () {
                     this.obj = new CloseButton();
                     $(this).append(this.obj.object);
                     console.log("ok",`click.${this.obj.id}`,`#${this.obj.id}`)
-                    $("body").on(`click.${this.obj.id}`,`#${this.obj.id}`,function(){
-
+                    $("body").on(`click.${this.obj.id}`,`#${this.obj.id}`,function(e){
+                        e.stopPropagation()
                         $(this).closest(`[box]`)[0].parameters.close(function(){});
                     })
 
