8b7e98d3fd428a395789ac6fcfdbabe1b3e72ea6
Volodymyr Cherniyevskyy <v.cherniyevskyy@gmail.com>
2024-08-27 09:28:53 +0000

    upd

--- modules/pages/login/index.js
+++ modules/pages/login/index.js
@@ -2,7 +2,7 @@ import HTMLObject from "../../../js/lib/html/HTMLObject.js";
 
 export default class PageLogin extends HTMLObject  {
 	 constructor() {
-        super("");
+        super("div");
         let _this = this;
         this.css = "/modules/pages/login/index.css";
         this.name = "page-login";        
