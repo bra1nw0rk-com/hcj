d3d555210d56d4e94919f7e2d972361c5cf300a0
Volodymyr Cherniyevskyy <v.cherniyevskyy@gmail.com>
2024-08-27 09:27:37 +0000

    upd

--- modules/items/buttons/close/index.js
+++ modules/items/buttons/close/index.js
@@ -2,10 +2,10 @@ import HTMLObject from "../../../../js/lib/html/HTMLObject.js";
 
 export default class CloseBtn extends HTMLObject  {
 	 constructor() {
-        super("");
+        super("span");
         let _this = this;
         this.css = "/modules/items/buttons/close/index.css";
-        this.name = "page-login";        
-        this.template = $(html`<i class="fa fa-close" class="btn-close"></i>`);        
+        this.name = "close-btn";        
+        this.template = $(html`<i class="fa fa-close"></i>`);        
     }    
 }
