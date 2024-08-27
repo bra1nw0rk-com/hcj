d2b7d26850a54e3cdf1687994ea3ec341d29ddce
Volodymyr Cherniyevskyy <v.cherniyevskyy@gmail.com>
2024-08-27 09:40:28 +0000

    upd

--- modules/items/buttons/modal-close/index.js
+++ modules/items/buttons/modal-close/index.js
@@ -1,11 +1,11 @@
 import HTMLObject from "../../../../js/lib/html/HTMLObject.js";
 
-export default class CloseBtn extends HTMLObject  {
+export default class ModalCloseBtn extends HTMLObject  {
 	 constructor() {
         super("span");
         let _this = this;
-        this.css = "/modules/items/buttons/close/index.css";
-        this.name = "close-btn";        
+        this.css = "/modules/items/buttons/modal-close/index.css";
+        this.name = "modal-close-btn";        
         this.template = $(html`<i class="fa fa-close"></i>`);        
     }    
 }
