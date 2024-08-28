ddacaa42c129f8c7cbb3830c5fb47a42da65b49d
Volodymyr Cherniyevskyy <v.cherniyevskyy@gmail.com>
2024-08-28 12:33:57 +0000

    upd

--- js/lib/html/Box.js
+++ js/lib/html/Box.js
@@ -29,8 +29,8 @@ export default class Box extends HTMLObject {
 			_this.#startPosition.x = null;
 			_this.#startPosition.y = null;
 		}).on("mousemove",function(e){
-			if(_this.#startPosition.x !== null && _this.#startPosition.y !== null){
-				console.log("move")
+			if(_this.#startPosition.x !== null && _this.#startPosition.y !== null){				
+				$(_this.object).css({top: _this.#startPosition.y - e.ClientY, left: _this.#startPosition.x - e.ClientX});
 			}
 			
 		})
