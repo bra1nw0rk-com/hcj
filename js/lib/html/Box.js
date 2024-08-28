3e71d80807ded23e17d1c5652600a7ff28985ce4
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-28 17:21:06 +0200

    upd

--- js/lib/html/Box.js
+++ js/lib/html/Box.js
@@ -34,7 +34,7 @@ export default class Box extends HTMLObject {
 			let obj = $(this).closest(`[box]`)[0].parameters;
 			obj.x = e.ClientX;
 			obj.y = e.ClientY;
-			console.log(obj)
+			console.log(obj,e.ClientX,e.ClientY)
 			$(this).addClass("cursor-move")			
 		}).on("mouseup",function(){
 			let obj = $(this).closest(`[box]`)[0].parameters;
