a1fbe27efc6e731a2edb4a2599ace8cf49dbe1b1
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-08-28 17:13:21 +0200

    upd

--- js/lib/html/Box.js
+++ js/lib/html/Box.js
@@ -36,7 +36,7 @@ export default class Box extends HTMLObject {
 	eventListener(){
 		let _this=this;
 		$(this.#head).on("mousedown",function(e){
-			let obj = $(this).closest(`box`)[0];
+			let obj = $(this).closest(`box`);
 			console.log(obj)
 			_this.x = e.ClientX;
 			_this.y = e.ClientY;
