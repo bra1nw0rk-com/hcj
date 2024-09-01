bccf7324e8c56971ab9b552609b4d344e44e4bfb
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-01 20:01:30 +0200

    upd

--- modules/time/time-calendar/index.js
+++ modules/time/time-calendar/index.js
@@ -1,11 +1,21 @@
 import HTMLObject from "../../../js/lib/html/HTMLObject.js";
 
-export default class PageLogin extends HTMLObject  {
+export default class TimeCalendar extends HTMLObject  {
+        timerHandler = null;
+        dateObj=$(`<div class="current-date"></div>`);
+        timeObj=$(`<div class="current-time"></div>`)
 	 constructor() {
         super("div");
         let _this = this;
         this.css = "/modules/time/time-calendar/index.css";
         this.name = "time-calendar";        
-        this.template = $(html``);        
+        this.template = $(html``);
+        this.object.append(this.dateObj)
+        this.object.append(this.timeObj)
+        this.timerHandler = setInterval(function(){
+                let currentDate = new Date();
+                _this.dateObj.html(currentDate.getDate() + "." + (currentDate.getMonth()+1)  + "." + currentDate.getFullYear())
+                _this.timeObj.html(currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds())
+        },1000);
     }    
 }
