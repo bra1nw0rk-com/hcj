95cbba7979fea2f444616bdc572a40545891aeef
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-02 22:09:49 +0200

    upd

--- js/lib/html/Box.js
+++ js/lib/html/Box.js
@@ -2,24 +2,24 @@ import HTMLObject from "./HTMLObject.js";
 
 export default class Box extends HTMLObject {
 	#head = $(html`<div class="head"></div>`);
-	#resizing=$(`
-		<span class="top-left-side"></span>
-		<span class="top-side"></span>
-		<span class="top-right-side"></span>
-		<span class="right-side"></span>
-		<span class="bottom-right-side"></span>
-		<span class="bottom-side"></span>
-		<span class="bottom-left-side"></span>
-		<span class="left-side"></span>
-	`)
+	// #resizing=$(`
+	// 	<span class="top-left-side"></span>
+	// 	<span class="top-side"></span>
+	// 	<span class="top-right-side"></span>
+	// 	<span class="right-side"></span>
+	// 	<span class="bottom-right-side"></span>
+	// 	<span class="bottom-side"></span>
+	// 	<span class="bottom-left-side"></span>
+	// 	<span class="left-side"></span>
+	// `)
 	#title = $(`<h2></h2>`);	
 	#content=$(`<div class="content"></div>`);
 
-	#resize={
-		class:"",
-		x:null,
-		y:null
-	}
+	// #resize={
+	// 	class:"",
+	// 	x:null,
+	// 	y:null
+	// }
 
 	set draggable(val){
 		if(val === true){
@@ -36,7 +36,6 @@ export default class Box extends HTMLObject {
 		super("div box");
 		this.unique = unique;
 		this.object.append(this.#head);
-		this.object.append(this.#resizing);
 		this.object.append(this.#content);
 		this.#head.append(this.#title);
 		this.object.attr("data-animated", "")
@@ -54,207 +53,207 @@ export default class Box extends HTMLObject {
 	// set moveY(val){
 	// 	this.#movePosition.y = val;
 	// }
-	get resizeX(){
-		return this.#resize.x;
-	}
-	set resizeX(val){
-		this.#resize.x = val;
-	}
-	get resizeY(){
-		return this.#resize.y;
-	}
-	set resizeY(val){
-		this.#resize.y = val;
-	}
-	get resizeClass(){
-		return this.#resize.class;
-	}
-	set resizeClass(val){
-		this.#resize.class = val;
-	}
+	// get resizeX(){
+	// 	return this.#resize.x;
+	// }
+	// set resizeX(val){
+	// 	this.#resize.x = val;
+	// }
+	// get resizeY(){
+	// 	return this.#resize.y;
+	// }
+	// set resizeY(val){
+	// 	this.#resize.y = val;
+	// }
+	// get resizeClass(){
+	// 	return this.#resize.class;
+	// }
+	// set resizeClass(val){
+	// 	this.#resize.class = val;
+	// }
 	eventListener(){
 		let _this=this;
-		$(this.#resizing).on('mousedown',function(e){
-			e.stopPropagation()
-			let elem = $(this).closest(`[box]`);
-			let obj = elem[0].parameters;
-			if(e.which === 1) {
-				if($(this).hasClass('top-side')){
-					obj.resizeClass = 'top-side'
-				}else if($(this).hasClass('right-side')){
-					obj.resizeClass = 'right-side'
-				}else if($(this).hasClass('bottom-side')){
-					obj.resizeClass = 'bottom-side'
-				}else if($(this).hasClass('left-side')){
-					obj.resizeClass = 'left-side'
-				}else if($(this).hasClass('top-left-side')){
-					obj.resizeClass = 'top-left-side'
-				}else if($(this).hasClass('top-right-side')){
-					obj.resizeClass = 'top-right-side'
-				}else if($(this).hasClass('bottom-right-side')){
-					obj.resizeClass = 'bottom-right-side'
-				}else if($(this).hasClass('bottom-left-side')){
-					obj.resizeClass = 'bottom-left-side'
-				}
-				obj.resizeX = e.pageX;
-				obj.resizeY = e.pageY;
-
-
-				$(`body`).on("mousemove.boxResizing", function (e) {
-					if (obj.resizeClass === "bottom-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left,
-							height: elem.outerHeight() + (e.pageY - obj.resizeY),
-							width: elem.outerWidth()
-						});
-					}else if (obj.resizeClass === "top-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top + (e.pageY - obj.resizeY),
-							left:elem.position().left,
-							height: elem.outerHeight() - (e.pageY - obj.resizeY),
-							width: elem.outerWidth()
-						});
-					}else if (obj.resizeClass === "left-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left + (e.pageX - obj.resizeX),
-							height: elem.outerHeight(),
-							width: elem.outerWidth() - (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "right-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left,
-							height: elem.outerHeight(),
-							width: elem.outerWidth() + (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "top-left-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top + (e.pageY - obj.resizeY),
-							left:elem.position().left + (e.pageX - obj.resizeX),
-							height: elem.outerHeight() - (e.pageY - obj.resizeY),
-							width: elem.outerWidth() - (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "top-right-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top + (e.pageY - obj.resizeY),
-							left:elem.position().left,
-							height: elem.outerHeight() - (e.pageY - obj.resizeY),
-							width: elem.outerWidth() + (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "bottom-right-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left,
-							height: elem.outerHeight() + (e.pageY - obj.resizeY),
-							width: elem.outerWidth() + (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "bottom-left-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left + (e.pageX - obj.resizeX),
-							height: elem.outerHeight() + (e.pageY - obj.resizeY),
-							width: elem.outerWidth() - (e.pageX - obj.resizeX),
-						});
-					}
-					obj.resizeX = e.pageX;
-					obj.resizeY = e.pageY;
-				})
-
-
-			}
-		}).on("mouseup",function(){
-			$(`body`).off('.boxResizing')
-			let obj = $(this).closest(`[box]`)[0].parameters;
-			obj.resizeX = null;
-			obj.resizeY = null;
-
-		}).on("mousemove",function(e){
-			e.stopPropagation()
-			let elem = $(this).closest(`[box]`);
-			let obj = elem[0].parameters;
-			if(obj.resizeX !== null && obj.resizeY !== null){
-				if($(this).hasClass(obj.resizeClass)) {
-					if (obj.resizeClass === "bottom-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left,
-							height: elem.outerHeight() + (e.pageY - obj.resizeY),
-							width: elem.outerWidth()
-						});
-					}else if (obj.resizeClass === "top-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top + (e.pageY - obj.resizeY),
-							left:elem.position().left,
-							height: elem.outerHeight() - (e.pageY - obj.resizeY),
-							width: elem.outerWidth()
-						});
-					}else if (obj.resizeClass === "left-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left + (e.pageX - obj.resizeX),
-							height: elem.outerHeight(),
-							width: elem.outerWidth() - (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "right-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left,
-							height: elem.outerHeight(),
-							width: elem.outerWidth() + (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "top-left-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top + (e.pageY - obj.resizeY),
-							left:elem.position().left + (e.pageX - obj.resizeX),
-							height: elem.outerHeight() - (e.pageY - obj.resizeY),
-							width: elem.outerWidth() - (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "top-right-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top + (e.pageY - obj.resizeY),
-							left:elem.position().left,
-							height: elem.outerHeight() - (e.pageY - obj.resizeY),
-							width: elem.outerWidth() + (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "bottom-right-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left,
-							height: elem.outerHeight() + (e.pageY - obj.resizeY),
-							width: elem.outerWidth() + (e.pageX - obj.resizeX),
-						});
-					}else if (obj.resizeClass === "bottom-left-side") {
-						elem.css({
-							transform:'none',
-							top: elem.position().top,
-							left:elem.position().left + (e.pageX - obj.resizeX),
-							height: elem.outerHeight() + (e.pageY - obj.resizeY),
-							width: elem.outerWidth() - (e.pageX - obj.resizeX),
-						});
-					}
-					obj.resizeX = e.pageX;
-					obj.resizeY = e.pageY;
-				}
-			}
-
-		})
+		// $(this.#resizing).on('mousedown',function(e){
+		// 	e.stopPropagation()
+		// 	let elem = $(this).closest(`[box]`);
+		// 	let obj = elem[0].parameters;
+		// 	if(e.which === 1) {
+		// 		if($(this).hasClass('top-side')){
+		// 			obj.resizeClass = 'top-side'
+		// 		}else if($(this).hasClass('right-side')){
+		// 			obj.resizeClass = 'right-side'
+		// 		}else if($(this).hasClass('bottom-side')){
+		// 			obj.resizeClass = 'bottom-side'
+		// 		}else if($(this).hasClass('left-side')){
+		// 			obj.resizeClass = 'left-side'
+		// 		}else if($(this).hasClass('top-left-side')){
+		// 			obj.resizeClass = 'top-left-side'
+		// 		}else if($(this).hasClass('top-right-side')){
+		// 			obj.resizeClass = 'top-right-side'
+		// 		}else if($(this).hasClass('bottom-right-side')){
+		// 			obj.resizeClass = 'bottom-right-side'
+		// 		}else if($(this).hasClass('bottom-left-side')){
+		// 			obj.resizeClass = 'bottom-left-side'
+		// 		}
+		// 		obj.resizeX = e.pageX;
+		// 		obj.resizeY = e.pageY;
+		//
+		//
+		// 		$(`body`).on("mousemove.boxResizing", function (e) {
+		// 			if (obj.resizeClass === "bottom-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight() + (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth()
+		// 				});
+		// 			}else if (obj.resizeClass === "top-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top + (e.pageY - obj.resizeY),
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight() - (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth()
+		// 				});
+		// 			}else if (obj.resizeClass === "left-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left + (e.pageX - obj.resizeX),
+		// 					height: elem.outerHeight(),
+		// 					width: elem.outerWidth() - (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "right-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight(),
+		// 					width: elem.outerWidth() + (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "top-left-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top + (e.pageY - obj.resizeY),
+		// 					left:elem.position().left + (e.pageX - obj.resizeX),
+		// 					height: elem.outerHeight() - (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth() - (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "top-right-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top + (e.pageY - obj.resizeY),
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight() - (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth() + (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "bottom-right-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight() + (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth() + (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "bottom-left-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left + (e.pageX - obj.resizeX),
+		// 					height: elem.outerHeight() + (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth() - (e.pageX - obj.resizeX),
+		// 				});
+		// 			}
+		// 			obj.resizeX = e.pageX;
+		// 			obj.resizeY = e.pageY;
+		// 		})
+		//
+		//
+		// 	}
+		// }).on("mouseup",function(){
+		// 	$(`body`).off('.boxResizing')
+		// 	let obj = $(this).closest(`[box]`)[0].parameters;
+		// 	obj.resizeX = null;
+		// 	obj.resizeY = null;
+		//
+		// }).on("mousemove",function(e){
+		// 	e.stopPropagation()
+		// 	let elem = $(this).closest(`[box]`);
+		// 	let obj = elem[0].parameters;
+		// 	if(obj.resizeX !== null && obj.resizeY !== null){
+		// 		if($(this).hasClass(obj.resizeClass)) {
+		// 			if (obj.resizeClass === "bottom-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight() + (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth()
+		// 				});
+		// 			}else if (obj.resizeClass === "top-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top + (e.pageY - obj.resizeY),
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight() - (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth()
+		// 				});
+		// 			}else if (obj.resizeClass === "left-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left + (e.pageX - obj.resizeX),
+		// 					height: elem.outerHeight(),
+		// 					width: elem.outerWidth() - (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "right-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight(),
+		// 					width: elem.outerWidth() + (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "top-left-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top + (e.pageY - obj.resizeY),
+		// 					left:elem.position().left + (e.pageX - obj.resizeX),
+		// 					height: elem.outerHeight() - (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth() - (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "top-right-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top + (e.pageY - obj.resizeY),
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight() - (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth() + (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "bottom-right-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left,
+		// 					height: elem.outerHeight() + (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth() + (e.pageX - obj.resizeX),
+		// 				});
+		// 			}else if (obj.resizeClass === "bottom-left-side") {
+		// 				elem.css({
+		// 					transform:'none',
+		// 					top: elem.position().top,
+		// 					left:elem.position().left + (e.pageX - obj.resizeX),
+		// 					height: elem.outerHeight() + (e.pageY - obj.resizeY),
+		// 					width: elem.outerWidth() - (e.pageX - obj.resizeX),
+		// 				});
+		// 			}
+		// 			obj.resizeX = e.pageX;
+		// 			obj.resizeY = e.pageY;
+		// 		}
+		// 	}
+		//
+		// })
 		// $(this.#head).on("mousedown",function(e){
 		// 	let elem = $(this).closest(`[box]`);
 		// 	let obj = elem[0].parameters;
--- js/lib/jquery/resizable.js
+++ js/lib/jquery/resizable.js
@@ -21,45 +21,133 @@ $(function () {
                         <span class="bottom-left-side"></span>
                         <span class="left-side"></span>
                     `)
-
-
-
-                    obj.movePosition={
+                    $(this).append(resizing);
+                    obj.resize={
+                        class:"",
                         x:null,
                         y:null
                     }
 
+                    obj.doResize=()=>{
+                        if (obj.resize.class === "bottom-side") {
+                            elem.css({
+                                transform:'none',
+                                top: elem.position().top,
+                                left:elem.position().left,
+                                height: elem.outerHeight() + (e.pageY - obj.obj.resize.y),
+                                width: elem.outerWidth()
+                            });
+                        }else if (obj.resize.class === "top-side") {
+                            elem.css({
+                                transform:'none',
+                                top: elem.position().top + (e.pageY - obj.obj.resize.y),
+                                left:elem.position().left,
+                                height: elem.outerHeight() - (e.pageY - obj.obj.resize.y),
+                                width: elem.outerWidth()
+                            });
+                        }else if (obj.resize.class === "left-side") {
+                            elem.css({
+                                transform:'none',
+                                top: elem.position().top,
+                                left:elem.position().left + (e.pageX - obj.obj.resize.x),
+                                height: elem.outerHeight(),
+                                width: elem.outerWidth() - (e.pageX - obj.obj.resize.x),
+                            });
+                        }else if (obj.resize.class === "right-side") {
+                            elem.css({
+                                transform:'none',
+                                top: elem.position().top,
+                                left:elem.position().left,
+                                height: elem.outerHeight(),
+                                width: elem.outerWidth() + (e.pageX - obj.obj.resize.x),
+                            });
+                        }else if (obj.resize.class === "top-left-side") {
+                            elem.css({
+                                transform:'none',
+                                top: elem.position().top + (e.pageY - obj.obj.resize.y),
+                                left:elem.position().left + (e.pageX - obj.obj.resize.x),
+                                height: elem.outerHeight() - (e.pageY - obj.obj.resize.y),
+                                width: elem.outerWidth() - (e.pageX - obj.obj.resize.x),
+                            });
+                        }else if (obj.resize.class === "top-right-side") {
+                            elem.css({
+                                transform:'none',
+                                top: elem.position().top + (e.pageY - obj.obj.resize.y),
+                                left:elem.position().left,
+                                height: elem.outerHeight() - (e.pageY - obj.obj.resize.y),
+                                width: elem.outerWidth() + (e.pageX - obj.obj.resize.x),
+                            });
+                        }else if (obj.resize.class === "bottom-right-side") {
+                            elem.css({
+                                transform:'none',
+                                top: elem.position().top,
+                                left:elem.position().left,
+                                height: elem.outerHeight() + (e.pageY - obj.obj.resize.y),
+                                width: elem.outerWidth() + (e.pageX - obj.obj.resize.x),
+                            });
+                        }else if (obj.resize.class === "bottom-left-side") {
+                            elem.css({
+                                transform:'none',
+                                top: elem.position().top,
+                                left:elem.position().left + (e.pageX - obj.obj.resize.x),
+                                height: elem.outerHeight() + (e.pageY - obj.obj.resize.y),
+                                width: elem.outerWidth() - (e.pageX - obj.obj.resize.x),
+                            });
+                        }
+                        obj.obj.resize.x = e.pageX;
+                        obj.obj.resize.y = e.pageY;
+                    }
 
-                    $(this).on("mousedown",function(e){
+                    $(resizing).on('mousedown',function(e){
+                        e.stopPropagation()
                         if(e.which === 1) {
-                            obj.movePosition.x = e.pageX;
-                            obj.movePosition.y = e.pageY;
-                            $(`body`).on("mousemove.boxMove", function (e) {
-                                elem.css({
-                                    top: elem.position().top + (e.pageY - obj.movePosition.y),
-                                    left: elem.position().left + (e.pageX - obj.movePosition.x)
-                                });
-                                obj.movePosition.x = e.pageX;
-                                obj.movePosition.y = e.pageY;
+                            if($(this).hasClass('top-side')){
+                                obj.resize.class = 'top-side'
+                            }else if($(this).hasClass('right-side')){
+                                obj.resize.class = 'right-side'
+                            }else if($(this).hasClass('bottom-side')){
+                                obj.resize.class = 'bottom-side'
+                            }else if($(this).hasClass('left-side')){
+                                obj.resize.class = 'left-side'
+                            }else if($(this).hasClass('top-left-side')){
+                                obj.resize.class = 'top-left-side'
+                            }else if($(this).hasClass('top-right-side')){
+                                obj.resize.class = 'top-right-side'
+                            }else if($(this).hasClass('bottom-right-side')){
+                                obj.resize.class = 'bottom-right-side'
+                            }else if($(this).hasClass('bottom-left-side')){
+                                obj.resize.class = 'bottom-left-side'
+                            }
+                            obj.obj.resize.x = e.pageX;
+                            obj.obj.resize.y = e.pageY;
+
+
+                            $(`body`).on("mousemove.boxResizing", function (e) {
+                                obj.doResize()
                             })
+
+
                         }
                     }).on("mouseup",function(){
-                        $(`body`).off('.boxMove')
-                        obj.movePosition.x = null;
-                        obj.movePosition.y = null;
+                        $(`body`).off('.boxResizing')
+                        obj.obj.resize.x = null;
+                        obj.obj.resize.y = null;
 
                     }).on("mousemove",function(e){
                         e.stopPropagation()
-                        if(obj.movePosition.x !== null && obj.movePosition.y !== null){
-                            elem.css({
-                                transform:'none',
-                                top: elem.position().top + (e.pageY - obj.movePosition.y),
-                                left: elem.position().left +  (e.pageX - obj.movePosition.x)
-                            });
-                            obj.movePosition.x = e.pageX;
-                            obj.movePosition.y = e.pageY;
+                        if(obj.obj.resize.x !== null && obj.obj.resize.y !== null){
+                            if($(this).hasClass(obj.resize.class)) {
+                                obj.doResize()
+                            }
                         }
+
                     })
+
+
+
+
+
+
                     return this;
                 });
             }
