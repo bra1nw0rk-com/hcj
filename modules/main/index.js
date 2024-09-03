e288f6d212e350ce1eee5ea0c9560e5fad68f4ab
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-03 22:14:11 +0200

    upd

--- modules/main/index.js
+++ modules/main/index.js
@@ -43,9 +43,10 @@ export default class Main extends HTMLObject {
 				});
 
 				$(_this.#running).find(`[data-obj-id]`).each(function(){
-					console.log($(`body`).find(`#${$(this).attr("data-obj-id")}`).length)
-					if($(`body`).find(`#${$(this).attr("data-obj-id")}`).length === 0){
+					if($(`#${$(this).attr("data-obj-id")}`).length === 0){
 						$(this).remove()
+					}else{
+						console.log($(`#${$(this).attr("data-obj-id")}`).length)
 					}
 				})
 			});
