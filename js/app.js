363f54e55aac3f11a3d20335643e40f8a6e8c6bd
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-03 19:33:04 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import Module from "./lib/core/Module.js";
 import LocalStorage from "./lib/core/LocalStorage.js";
 import "/js/lib/main.js";
 import USER from "./lib/core/USER.js";
-import NOTIFICATION from "./lib/core/NOTIFICATION";
+import NOTIFICATION from "./lib/core/NOTIFICATION.js";
 
 globalThis.Module = Module;
 globalThis.storage = new LocalStorage("ws");
