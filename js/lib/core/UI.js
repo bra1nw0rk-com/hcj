b24a6f53be200603ffc5c42499f7f25e67269eb5
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-04 22:30:49 +0200

    upd

--- js/lib/core/UI.js
+++ js/lib/core/UI.js
@@ -168,7 +168,7 @@ export default class UI {
 	}
 
     minimizeModal(selector) {
-		$(selector)[0].parameters.lastPosition.x =   ($(selector).position().top  / $(`body`).height()) * 100;
+		$(selector)[0].parameters.lastPosition.x =   (($(selector).position().top +2)  / $(`body`).height()) * 100;
 		$(selector)[0].parameters.lastPosition.y =  ($(selector).position().left / $(`body`).width()) * 100;
 		let iconLeft = ($(`[data-obj-id="${$(selector).attr('id')}"]`).position().left / $(`body`).width()) * 100;
 
