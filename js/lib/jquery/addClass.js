8936f759a994c5e99d85723c1af49c8680af45fb
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-05 21:48:00 +0200

    upd

--- js/lib/jquery/addClass.js
+++ js/lib/jquery/addClass.js
@@ -9,18 +9,12 @@ $(function () {
                     let classArr = classNames.split(/\s+/);
                     $.each(classArr, function(index, value){
                         if(!_this.hasClass(value)) {
-                            console.log(_this[0].classList)
                             _this[0].classList.add(value);
-
                             _this.trigger('addClass', {
                                 class: value
                             })
-                            console.log(`Class ${value} added to`, _this)
                         }
                     });
-
-
-
                     return this;
                 });
             }
