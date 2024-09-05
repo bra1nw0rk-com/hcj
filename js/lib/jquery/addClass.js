f1f261dd7ec2a720c0686e9ed5d58c6d03da18d4
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-05 21:46:08 +0200

    upd

--- js/lib/jquery/addClass.js
+++ js/lib/jquery/addClass.js
@@ -5,17 +5,17 @@ $(function () {
             if ($(this).length > 0) {
                 return $(this).each(function (e, i) {
                     //$(this).addClass(className)
-
+                    let _this = $(this)
                     let classArr = classNames.split(/\s+/);
                     $.each(classArr, function(index, value){
-                        if(!$(this).hasClass(value)) {
-                            console.log($(this)[0].classList)
-                            $(this)[0].classList.add(value);
+                        if(!_this.hasClass(value)) {
+                            console.log(_this[0].classList)
+                            _this[0].classList.add(value);
 
-                            $(this).trigger('addClass', {
+                            _this.trigger('addClass', {
                                 class: value
                             })
-                            console.log(`Class ${value} added to`, $(this))
+                            console.log(`Class ${value} added to`, _this)
                         }
                     });
 
