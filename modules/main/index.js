501db93bc0943b653b0fb3adca666239788d53a3
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-07 09:39:13 +0200

    upd

--- modules/main/index.js
+++ modules/main/index.js
@@ -50,7 +50,14 @@ export default class Main extends HTMLObject {
 					newObj.object.addClass('clickable fadeIn selected')
 					newObj.object.attr("data-animated","");
 					//newObj.attr("data-title",$(this)[0].parameters.title)
-					newObj.tooltip =$(this)[0].parameters.title
+					let boxObj = $(this).clone();
+					boxObj.attr("miniature-id",$(this).attr("id"))
+					boxObj.removeAttr("id")
+					boxObj.removeAttr("style")
+					boxObj.unbind();
+					boxObj.find(`*`).unbind();
+
+					newObj.tooltip =/*$(this)[0].parameters.title*/ boxObj.get()
 					$(this).attr("run-added","");
 					$(`[data-obj-id]`).removeClass("selected")
 					newObj.init()
