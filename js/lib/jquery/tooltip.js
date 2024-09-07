35bf0b8d3035157d5f7c7a1f6cdabcfdbf4da472
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-07 08:26:44 +0200

    upd

--- js/lib/jquery/tooltip.js
+++ js/lib/jquery/tooltip.js
@@ -4,7 +4,7 @@
  * {@link https://github.com/bra1nw0rk-com/hcj GitHub}.
  * {@link https://www.linkedin.com/in/volodymyr-cherniyevskyy-24962b22b LinkedIn}
  **/
-import HTMLObject from "../html/HTMLObject";
+import HTMLObject from "../html/HTMLObject.js";
 
 $(function () {
     $.fn.extend({
