18c0bbc3dde952fc063fc3ae582ba24c6f46963f
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-07 09:33:21 +0200

    upd

--- js/lib/jquery/tooltip.js
+++ js/lib/jquery/tooltip.js
@@ -27,7 +27,7 @@ $(function () {
                             if(_this.tooltip !== "") {
                                 tooltipObj.object.css({
                                     left:`calc(${e.pageX}px)`,
-                                    top:`calc(${e.pageY}px - 20px)`
+                                    top:`calc(${e.pageY}px - 40px)`
 
                                 })
                                 $(`body`).append(tooltipObj.get())
