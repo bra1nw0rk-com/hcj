9ac2525cec820f40d9030325b6ce8660399ad332
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-08 13:40:33 +0200

    upd

--- js/lib/jquery/draggable.js
+++ js/lib/jquery/draggable.js
@@ -36,7 +36,7 @@ $(function () {
                                     });
                                     obj.movePosition.x = e.pageX;
                                     obj.movePosition.y = e.pageY;
-                                    obj.parameters.lastPosition.y =  /*elem.position().top*/ elem.offset().top
+                                    obj.parameters.lastPosition.y =  /*elem.position().top*/ elem.offset().top +8
                                     obj.parameters.lastPosition.x =  elem.position().left
                                 }                                
                             })
