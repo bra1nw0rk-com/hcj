2a98c957043f3ff5ee165cc4e05620e6b2debd1b
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-08 13:45:36 +0200

    upd

--- js/lib/core/UI.js
+++ js/lib/core/UI.js
@@ -208,7 +208,16 @@ export default class UI {
 				zoom: '100%',
 				left: `${$(selector)[0].parameters.lastPosition.x}px`,
 
-			},500,function(){}
+			},500,function(){
+				$(selector).animate(
+					{
+						top: `${$(selector)[0].parameters.lastPosition.y}px`,
+
+					},500,function(){
+
+					}
+				);
+			}
 		);
 	}
 }
