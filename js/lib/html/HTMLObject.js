7aaf383db882e9f8f1b7335296cd7e4060509791
Volodymyr Cherniyevskyy <v.cherniyevskyy@gmail.com>
2024-09-13 10:37:59 +0000

    upd

--- js/lib/html/HTMLObject.js
+++ js/lib/html/HTMLObject.js
@@ -105,6 +105,7 @@ export default class HTMLObject extends CustomEvents {
 	init(){
 		let _this = this
 		$("body").off(`.${this.id}`)
+		console.log(`css: ${this.css}`, this.id)
 		if(this.css === ""){
 			this.object.trigger('cssLoaded')			
 		}
