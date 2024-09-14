e5b16a5cbd802d067391816c97734b3131b17a9d
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-14 21:45:19 +0200

    upd

--- modules/main/box-manager/index.js
+++ modules/main/box-manager/index.js
@@ -53,13 +53,8 @@ export default class BoxManager extends HTMLObject  {
                         _this.showed = true;
                         WS.ui.effects.fadeIn(_this.object,function(){
                             if(_this.object.css('display')!=="none") {
-                                _this.saveState()
                                 _this.object.css({
-                                    left: `${_this.lastPosition.x}px`,
-                                    top: `${_this.lastPosition.y}px`,
-                                    height: `${_this.size.height}px`,
-                                    width: `${_this.size.width}px`,
-                                    transform: 'none'
+                                    cursor:"inherit"
                                 })
                             }
                         })
