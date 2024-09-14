9af8107c0c791803423d0f2a33d4d4352b66baab
Cherniyevskyy Volodymyr <v.cherniyevskyy@gmail.com>
2024-09-14 16:05:17 +0200

    upd

--- modules/demo/items/index.js
+++ modules/demo/items/index.js
@@ -84,12 +84,13 @@ export default class ItemsDemo extends Box {
                         <option value="value4">Value 4</option>
                     </select>
                 </label>
+                <!--
                 <label> Textarea:
                     <textarea id="textarea0" name="item_textarea" rows="4" cols="50">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </textarea>
                 </label>
-                <!--
+                
                 <input type="color">
                 <input type="date">
                 <input type="datetime-local">
